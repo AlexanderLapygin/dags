4b4086fc431bd0382ef87e240b18d977a587fd37
AlexanderLapygin <AlexanderLapygin@gmail.com>
2020-09-23 01:57:41 +0300

    feat: add uid-counter

--- packages/uid-counter/src/__tests__/uid-counter.test.ts
+++ packages/uid-counter/src/__tests__/uid-counter.test.ts
@@ -1,32 +1,32 @@
-import { UUID } from '../uid';
+import { UIDCounter } from '../uid-counter';
 
 describe('UID', () => {
   describe('constructor', () => {
     it('Should create UID successfully', () => {
-      expect(() => new UUID()).not.toThrow();
+      expect(() => new UIDCounter()).not.toThrow();
     });
     it('Should create different UIDs', () => {
-      expect(new UUID()).not.toEqual(new UUID());
+      expect(new UIDCounter()).not.toEqual(new UIDCounter());
     });
   });
   describe('equals', () => {
     it('Should return false on different UIDs', () => {
-      expect(new UUID().equals(new UUID())).toBe(false);
+      expect(new UIDCounter().equals(new UIDCounter())).toBe(false);
     });
 
     it('Should be true on the same UIDs', () => {
-      const uid1 = new UUID();
+      const uid1 = new UIDCounter();
       const uid2 = uid1;
       expect(uid1.equals(uid2)).toBe(true);
     });
   });
   describe('newUID', () => {
-    const uuid = new UUID();
+    const uid = new UIDCounter();
     it('Should return unique ids', () => {
-      expect(uuid.newUID() == uuid.newUID()).toBe(false);
+      expect(uid.newUID() == uid.newUID()).toBe(false);
     });
     it('Should return a value of type \'object\'', () => {
-      expect(typeof uuid.newUID()).toBe('object');
+      expect(typeof uid.newUID()).toBe('object');
     });
   });
 });
---
+++ packages/uid-counter/src/uid-counter.ts
@@ -0,0 +1,22 @@
+import { UID } from '@dags/dag';
+
+/**
+ * Implements the UID interface that Dag requires.
+ * @class Unique id.
+ */
+export class UIDCounter implements UID {
+  private static _counter = 0;
+  private readonly _id: number;
+
+  constructor() {
+    this._id = UIDCounter._counter++;
+  }
+
+  newUID(): UID {
+    return new UIDCounter();
+  }
+
+  equals(uid: UID): boolean {
+    return uid instanceof UIDCounter && this._id === uid._id;
+  }
+}
