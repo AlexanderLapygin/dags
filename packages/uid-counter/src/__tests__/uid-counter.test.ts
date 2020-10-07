516a8c35373af78e3f9e49960f5aeea94cd4d688
AlexanderLapygin <AlexanderLapygin@gmail.com>
2020-10-07 03:42:38 +0300

    test: check fail

--- packages/uid-counter/src/__tests__/uid-counter.test.ts
+++ packages/uid-counter/src/__tests__/uid-counter.test.ts
@@ -11,7 +11,7 @@ describe('UID', () => {
   });
   describe('equals', () => {
     it('Should return false on different UIDs', () => {
-      expect(new UIDCounter().equals(new UIDCounter())).toBe(false);
+      expect(new UIDCounter().equals(new UIDCounter())).toBe(true);
     });
 
     it('Should be true on the same UIDs', () => {
