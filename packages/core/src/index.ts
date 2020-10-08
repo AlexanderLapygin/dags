d9ee841f06f4b3533425020c2902b3a5a5d05ea0
AlexanderLapygin <AlexanderLapygin@gmail.com>
2020-10-08 22:09:30 +0300

    feat: add examples package

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,1 +1,3 @@
 export * from '@dags/dag'
+export * from '@dags/uid-counter'
+export * from '@dags/uid-uuid'
---
+++ packages/examples/index.ts
@@ -0,0 +1,20 @@
+import { Dag, UUID } from '@dags/core';
+
+const dag = new Dag(new UUID());
+
+const parent = dag.newNode();
+const child = dag.newNode();
+console.log(dag.getNodes().size)
+
+console.log(dag.getChildren(parent).size)
+console.log(dag.getParents(child).size)
+
+dag.setParenthood(child, parent);
+
+console.log(dag.getChildren(parent).size)
+console.log(dag.getParents(child).size)
+
+dag.removeParenthood(child, parent);
+
+console.log(dag.getChildren(parent).size)
+console.log(dag.getParents(child).size)
