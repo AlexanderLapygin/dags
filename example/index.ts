4c87b2e8a7c0ad19dd1227205aa8310684656a9e
AlexanderLapygin <AlexanderLapygin@gmail.com>
2020-10-12 17:00:53 +0300

    refactor: formatting

--- example/index.ts
+++ example/index.ts
@@ -4,17 +4,17 @@ const dag = new Dag(new UUID());
 
 const parent = dag.newNode();
 const child = dag.newNode();
-console.log(dag.getNodes().size)
+console.log(dag.getNodes().size);
 
-console.log(dag.getChildren(parent).size)
-console.log(dag.getParents(child).size)
+console.log(dag.getChildren(parent).size);
+console.log(dag.getParents(child).size);
 
 dag.setParenthood(child, parent);
 
-console.log(dag.getChildren(parent).size)
-console.log(dag.getParents(child).size)
+console.log(dag.getChildren(parent).size);
+console.log(dag.getParents(child).size);
 
 dag.removeParenthood(child, parent);
 
-console.log(dag.getChildren(parent).size)
-console.log(dag.getParents(child).size)
+console.log(dag.getChildren(parent).size);
+console.log(dag.getParents(child).size);
