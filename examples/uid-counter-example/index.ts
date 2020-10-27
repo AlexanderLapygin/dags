890ba78b0a4c4d9f181271ffbd1b93372d7e6d4b
AlexanderLapygin <AlexanderLapygin@gmail.com>
2020-10-28 02:07:06 +0300

    docs: add examples for uid-uuid and uid-counter

---
+++ examples/uid-counter-example/index.ts
@@ -0,0 +1,8 @@
+import { UIDCounter } from '@dags/core'
+
+const uuid1 = new UIDCounter()
+const uuid2 = new UIDCounter()
+
+console.log(uuid1)
+console.log(uuid2)
+console.log(uuid1.equals(uuid2))
---
+++ examples/uid-uuid-example/index.ts
@@ -0,0 +1,8 @@
+import { UUID } from '@dags/core'
+
+const uuid1 = new UUID()
+const uuid2 = new UUID()
+
+console.log(uuid1)
+console.log(uuid2)
+console.log(uuid1.equals(uuid2))
