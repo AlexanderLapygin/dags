6ae5d9cfd7be9b56e5aeb98f0594437f58025701
AlexanderLapygin <AlexanderLapygin@gmail.com>
2020-11-21 13:24:25 +0300

    chore: jest config update

--- packages/jest.config.js
+++ packages/jest.config.js
@@ -1,6 +1,13 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  transform: { '.(ts|tsx)$': 'ts-jest/dist' },
   transformIgnorePatterns: ['[/\\\\]node_modules[/\\\\].+\\.(js|jsx)$'],
-  testPathIgnorePatterns: ['/dist/', '/node_modules/']
+  testPathIgnorePatterns: ['/dist/', '/node_modules/'],
+  globals: {
+    'ts-jest': {
+      tsconfig: 'tsconfig.json'
+    }
+  },
+  watchPlugins: ['jest-watch-typeahead/filename', 'jest-watch-typeahead/testname']
 }
