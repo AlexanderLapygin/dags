85b3cb0cd4e64c0a941f5a373a2de00604543491
AlexanderLapygin <AlexanderLapygin@gmail.com>
2021-02-17 16:02:43 +0300

    feat: new example

---
+++ examples/dag-example-new/index.ts
@@ -0,0 +1,26 @@
+import { Dag } from '@dags/dag'
+
+const dag = new Dag()
+
+console.log('Dag example:')
+
+const parent = dag.newNode()
+const child = dag.newNode()
+
+console.log(parent.equals(parent))
+console.log(parent.equals(child))
+
+console.log(dag.getNodes().size)
+
+console.log(dag.getChildren(parent).size)
+console.log(dag.getParents(child).size)
+
+dag.setParenthood(parent, child)
+
+console.log(dag.getChildren(parent).size)
+console.log(dag.getParents(child).size)
+
+dag.removeParenthood(parent, child)
+
+console.log(dag.getChildren(parent).size)
+console.log(dag.getParents(child).size)
